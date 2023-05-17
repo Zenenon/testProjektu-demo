27c2afba9f371ff25908473719f872fb7d60ac89
Zenenon <272938@student.pwr.edu.pl>
2023-05-17 22:21:19 +0200

    mamma mia

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.print("I hate java");
+        System.out.println("I hate java, noiz");
+    }
+    //ciekawe co można tu zepsuć
+
+}
